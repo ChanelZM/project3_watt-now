0dc5b9f81db462f35c72819f7c615619df63e4b9
Chanel Mepschen <chanelmepschen856@hotmail.com>
2017-05-11 12:24:26 +0200

    Added favicon, apple toch icon, app can be added to the homescreen, has offline page

--- public/sw.js
+++ public/sw.js
@@ -1,25 +1,27 @@
 var CACHE_NAME = 'watt-now-cache-v1';
+var CACHE_PAGES_NAME = 'watt-now-pages';
 var pageCache = [
     '/',
     '/css/style.css',
     '/html/offline.html'
 ];
 
+//Create cache when serviceworker is being installed.
 self.addEventListener('install', function(event){
-    eveny.waitUntil(
+    event.waitUntil(
         caches.open(CACHE_NAME)
         .then(function(cache){
             return cache.addAll(pageCache);
         }).then(self.skipWaiting()));
 });
 
-self.addEventListener('fetch'), function(event){
+self.addEventListener('fetch', function(event){
     var request = event.request;
     if(request.mode === 'navigate'){
         event.respondWith(
             fetch(request)
             .then(function(response){
-                return cachePage(request, response));
+                return cachePage(request, response);
             })
             .catch(function(err){
                 return getCachedRequest(request);
@@ -40,3 +42,32 @@ self.addEventListener('fetch'), function(event){
         );
     }
 });
+
+function fetchCoreFile(url){
+    return caches.open(CACHE_NAME)
+    .then(function(cache){
+        return cache.match(url);
+    })
+    .then(function(response){
+        return response ? response : Promise.reject();
+    });
+}
+
+function getCachedPage(request){
+    return caches.open(CACHE_PAGES_NAME)
+    .then(function(cache){
+        return cache.match(request);
+    })
+    .then(function(response){
+        return response ? response : Promise.reject();
+    });
+}
+
+function cachePage(request, response){
+    var clonedResponse = response.clone();
+    caches.open(CACHE_PAGES_NAME)
+    .then(function(cache){
+        return cache.put(request, clonedResponse);
+    });
+    return response;
+}
