ad3262cc0504dafd2f97e51fd51b9c4c48249a0b
Shyanta Vleugel <shyantavleugel@gmail.com>
2017-05-12 13:00:46 +0200

    Add Data Arrays

---
+++ public/js/data.js
@@ -0,0 +1,390 @@
+var momentOne = [
+	{
+		"red" : 77,
+		"orange": 10,
+		"yellow": 0,
+		"green": 0
+	},
+	{
+		"red" : 4,
+		"orange": 3,
+		"yellow": 25,
+		"green": 0
+	},
+	{
+		"red" : 3,
+		"orange": 3,
+		"yellow": 4,
+		"green": 5
+	},
+	{
+		"red" : 10,
+		"orange": 55,
+		"yellow": 3,
+		"green": 0
+	}
+];
+var momentTwo = [
+	{
+		"red" : 84,
+		"orange": 5,
+		"yellow": 6,
+		"green": 0
+	},
+	{
+		"red" : 4,
+		"orange": 5,
+		"yellow": 5,
+		"green": 16
+	},
+	{
+		"red" : 5,
+		"orange": 4,
+		"yellow": 4,
+		"green": 8
+	},
+	{
+		"red" : 4,
+		"orange": 3,
+		"yellow": 25,
+		"green": 0
+	}
+];
+var momentThree = [
+	{
+		"red" : 20,
+		"orange": 10,
+		"yellow": 45,
+		"green": 15
+	},
+	{
+		"red" : 4,
+		"orange": 3,
+		"yellow": 25,
+		"green": 0
+	},
+	{
+		"red" : 4,
+		"orange": 5,
+		"yellow": 5,
+		"green": 16
+	},
+	{
+		"red" : 7,
+		"orange": 2,
+		"yellow": 28,
+		"green": 0
+	}
+];
+var momentFour = [
+	{
+		"red" : 5,
+		"orange": 4,
+		"yellow": 4,
+		"green": 8
+	},
+	{
+		"red" : 10,
+		"orange": 55,
+		"yellow": 3,
+		"green": 0
+	},
+	{
+		"red" : 5,
+		"orange": 5,
+		"yellow": 35,
+		"green": 0
+	},
+	{
+		"red" : 20,
+		"orange": 10,
+		"yellow": 45,
+		"green": 15
+	}
+];
+var momentFive = [
+	{
+		"red" : 3,
+		"orange": 3,
+		"yellow": 4,
+		"green": 5
+	},
+	{
+		"red" : 10,
+		"orange": 73,
+		"yellow": 0,
+		"green": 0
+	},
+	{
+		"red" : 20,
+		"orange": 10,
+		"yellow": 45,
+		"green": 15
+	},
+	{
+		"red" : 4,
+		"orange": 5,
+		"yellow": 5,
+		"green": 16
+	}
+];
+var momentSix = [
+	{
+		"red" : 5,
+		"orange": 4,
+		"yellow": 4,
+		"green": 8
+	},
+	{
+		"red" : 77,
+		"orange": 10,
+		"yellow": 0,
+		"green": 0
+	},
+	{
+		"red" : 10,
+		"orange": 73,
+		"yellow": 0,
+		"green": 0
+	},
+	{
+		"red" : 4,
+		"orange": 3,
+		"yellow": 25,
+		"green": 0
+	}
+];
+var momentSeven = [
+	{
+		"red" : 4,
+		"orange": 5,
+		"yellow": 5,
+		"green": 16
+	},
+	{
+		"red" : 84,
+		"orange": 5,
+		"yellow": 6,
+		"green": 0
+	},
+	{
+		"red" : 77,
+		"orange": 10,
+		"yellow": 0,
+		"green": 0
+	},
+	{
+		"red" : 5,
+		"orange": 5,
+		"yellow": 35,
+		"green": 0
+	}
+];
+var momentEight = [
+	{
+		"red" : 4,
+		"orange": 3,
+		"yellow": 25,
+		"green": 0
+	},
+	{
+		"red" : 95,
+		"orange": 0,
+		"yellow": 0,
+		"green": 0
+	},
+	{
+		"red" : 10,
+		"orange": 73,
+		"yellow": 0,
+		"green": 0
+	},
+	{
+		"red" : 20,
+		"orange": 10,
+		"yellow": 45,
+		"green": 15
+	}
+];
+var momentNine = [
+	{
+		"red" : 20,
+		"orange": 10,
+		"yellow": 45,
+		"green": 15
+	},
+	{
+		"red" : 10,
+		"orange": 55,
+		"yellow": 3,
+		"green": 0
+	},
+	{
+		"red" : 77,
+		"orange": 10,
+		"yellow": 0,
+		"green": 0
+	},
+	{
+		"red" : 10,
+		"orange": 73,
+		"yellow": 0,
+		"green": 0
+	}
+];
+var momentTen = [
+	{
+		"red" : 20,
+		"orange": 10,
+		"yellow": 45,
+		"green": 15
+	},
+	{
+		"red" : 5,
+		"orange": 5,
+		"yellow": 35,
+		"green": 0
+	},
+	{
+		"red" : 84,
+		"orange": 5,
+		"yellow": 6,
+		"green": 0
+	},
+	{
+		"red" : 10,
+		"orange": 55,
+		"yellow": 3,
+		"green": 0
+	}
+];
+var momentEleven = [
+	{
+		"red" : 10,
+		"orange": 55,
+		"yellow": 3,
+		"green": 0
+	},
+	{
+		"red" : 20,
+		"orange": 10,
+		"yellow": 45,
+		"green": 15
+	},
+	{
+		"red" : 95,
+		"orange": 0,
+		"yellow": 0,
+		"green": 0
+	},
+	{
+		"red" : 5,
+		"orange": 5,
+		"yellow": 35,
+		"green": 0
+	}
+];
+var momentTwelve = [
+	{
+		"red" : 10,
+		"orange": 73,
+		"yellow": 0,
+		"green": 0
+	},
+	{
+		"red" : 10,
+		"orange": 55,
+		"yellow": 3,
+		"green": 0
+	},
+	{
+		"red" : 77,
+		"orange": 10,
+		"yellow": 0,
+		"green": 0
+	},
+	{
+		"red" : 4,
+		"orange": 3,
+		"yellow": 25,
+		"green": 0
+	}
+];
+var momentThirteen = [
+	{
+		"red" : 77,
+		"orange": 10,
+		"yellow": 0,
+		"green": 0
+	},
+	{
+		"red" : 14,
+		"orange": 67,
+		"yellow": 0,
+		"green": 0
+	},
+	{
+		"red" : 10,
+		"orange": 55,
+		"yellow": 3,
+		"green": 0
+	},
+	{
+		"red" : 20,
+		"orange": 10,
+		"yellow": 45,
+		"green": 15
+	}
+];
+var momentFourteen = [
+	{
+		"red" : 10,
+		"orange": 73,
+		"yellow": 0,
+		"green": 0
+	},
+	{
+		"red" : 20,
+		"orange": 10,
+		"yellow": 45,
+		"green": 15
+	},
+	{
+		"red" : 4,
+		"orange": 3,
+		"yellow": 25,
+		"green": 0
+	},
+	{
+		"red" : 5,
+		"orange": 5,
+		"yellow": 35,
+		"green": 0
+	}
+];
+var momentFifteen = [
+	{
+		"red" : 84,
+		"orange": 5,
+		"yellow": 6,
+		"green": 0
+	},
+	{
+		"red" : 7,
+		"orange": 2,
+		"yellow": 28,
+		"green": 0
+	},
+	{
+		"red" : 4,
+		"orange": 6,
+		"yellow": 4,
+		"green": 12
+	},
+	{
+		"red" : 14,
+		"orange": 67,
+		"yellow": 0,
+		"green": 0
+	}
+];
